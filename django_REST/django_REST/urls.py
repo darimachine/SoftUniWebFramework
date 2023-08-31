ca75e79e568e54adca8c71fbadbef91dacce74a2
Dari <serhi1334@gmail.com>
2023-08-31 16:08:18 +0300

    Added little bit of js

--- django_REST/django_REST/urls.py
+++ django_REST/django_REST/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('', include('web.urls')),
 ]
